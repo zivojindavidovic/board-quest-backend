bd9664335a8e2ae4764417784d8575cddd938f77
zivojindavidovic <zivojin.davidovic2001@gmail.com>
2026-02-26 20:19:18 +0100

    feat: change response following api best practices

--- app/Http/Resources/ListGameResource.php
+++ app/Http/Resources/ListGameResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources;
 
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -15,5 +16,14 @@ class ListGameResource extends JsonResource
     public function toArray(Request $request): array
     {
         return parent::toArray($request);
+    }
+
+    public function toResponse($request): JsonResponse
+    {
+        $response = parent::toResponse($request);
+        $data = ['success' => true] + $response->getData(true);
+        $response->setData($data);
+
+        return $response;
     }
 }
