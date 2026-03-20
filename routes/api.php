1e6bd752a0293d49137d5167920d79f5c776457f
zivojindavidovic <zivojin.davidovic2001@gmail.com>
2026-03-20 21:13:19 +0100

    feat: added post route for creating club to api routes

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\ClubsController;
 use App\Http\Controllers\GamesController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/v1/games', [GamesController::class, 'list']);
 Route::get('/v1/games/{uuid}', [GamesController::class, 'listByUUID']);
+
+Route::post('/v1/clubs', [ClubsController::class, 'register']);
