00043e724d9b455a22a4b9c2346ddf1f5e0268cc
zivojindavidovic <zivojin.davidovic2001@gmail.com>
2026-03-20 21:14:11 +0100

    feat: added Game model relations

--- app/Models/Game.php
+++ app/Models/Game.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Game extends Model
 {
@@ -12,4 +13,9 @@ class Game extends Model
 
     public const string KEY_ID = 'id';
     public const string KEY_BGG_ID = 'bgg_id';
+
+    public function clubs(): BelongsToMany
+    {
+        return $this->belongsToMany(Club::class)->withTimestamps();
+    }
 }
